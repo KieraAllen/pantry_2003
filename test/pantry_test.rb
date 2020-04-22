2d027fd1b543094e404d456640d8270e8fc53a8e
Kiera Allen <kieraaallen@gmail.com>
2020-04-22 09:43:50 -0600

    Test it can restock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -20,17 +20,16 @@ class PantryTest < Minitest::Test
   end
 
   def test_it_can_check_for_ingredient
-    # require "pry"; binding.pry
     assert_equal 0, @pantry.stock_check(@ingredient1)
   end
-end
 
-# pry(main)> pantry.restock(ingredient1, 5)
-#
-# pry(main)> pantry.restock(ingredient1, 10)
-#
-# pry(main)> pantry.stock_check(ingredient1)
-# # => 15
+  def test_it_can_restock
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+  end
+end
 #
 # pry(main)> pantry.restock(ingredient2, 7)
 #
