a9114307d8ca51fe2238d88a82c750082727de34
Kiera Allen <kieraaallen@gmail.com>
2020-04-22 09:49:39 -0600

    Remove extraneous pantry from test setup

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,7 +8,7 @@ class PantryTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-    @pantry = pantry = Pantry.new
+    @pantry = Pantry.new
   end
 
   def test_it_exists
@@ -30,7 +30,7 @@ class PantryTest < Minitest::Test
     assert_equal 15, @pantry.stock_check(@ingredient1)
 
     @pantry.restock(@ingredient2, 7)
-    
+
     assert_equal 7, @pantry.stock_check(@ingredient2)
   end
 end
